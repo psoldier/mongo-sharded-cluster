7a7ef024efd77db23e9bd799b320bcfb80324e45
Pablo Soldi <soldi.pablo@gmail.com>
2017-04-01 00:55:31 -0300

    Add charts, favicon & general clean code

--- app.rb
+++ app.rb
@@ -6,10 +6,13 @@ require_relative 'config/application'
 require_relative 'models/archive'
 
 class App < Hobbit::Base
-  include Hobbit::Mote
-  use Rack::Static, root: 'public', urls: ['/css','/fonts','/js']
+  include Hobbit::Render
+  use Rack::Static, root: 'public', urls: ['/css','/fonts','/js','/images']
 
   get '/' do
+    @pablo = "pabloKpo"
+    @inserts_data = [{ x: 1, y: 2.1 }, { x: 3.3, y: 3.9 }, { x: 5.2, y: 7.1 }]
+    @finds_data = [{ x: 2, y: 0 }, { x: 3, y: 2 }, { x: 5, y: 10 }]
     render 'index'
   end
 
@@ -21,8 +24,10 @@ class App < Hobbit::Base
       archivo = Archive.new(path_filename).save
       save_last_insertion_stadistics(get_last_query_time)
       #recupero el archivo
-      Archive.find(archivo.name)
+      archive = Archive.find(archivo.name)
       save_last_find_stadistics(get_last_query_time)
+      #check same file saved
+      raise "Archivo mal guardado" unless archive.ok?
     end
 =end
   end
---
+++ public/js/stadistics.js
@@ -0,0 +1,40 @@
+var ctx = document.getElementById("myChart");
+
+var data = {
+  datasets: [
+    {
+      label: '# Tiempo de respuesta en la inserción',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      data: inser_data
+    },
+    {
+      label: '# Tiempo de respuesta en la búsqueda',
+      borderColor: 'rgba(153, 102, 255, 1)',
+      backgroundColor: 'rgba(153, 102, 255, 0.2)',
+      data: find_data
+    }
+  ]
+};
+var chartInstanceDifferentHoverMode = new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: {
+    scales: {
+      xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Bytes'
+        },
+        type: 'linear',
+        position: 'bottom'
+      }],
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Milisegundos'
+        }
+      }]
+    }
+  }
+});
