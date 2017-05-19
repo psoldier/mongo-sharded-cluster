740e9e934eb259ff7c3b44817e5dd7f008b83905
pablo <soldi.pablo@gmail.com>
2017-05-19 05:33:13 -0300

    3shards results

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
 Mongo::Logger.logger = ::Logger.new('mongo.log')
 
-DB = Mongo::Client.new([ '163.10.33.222' ], database: 'tesina_tres_shards', connect: :sharded).database
-SHARD_IPS = ['163.10.33.223','163.10.33.217','163.10.33.227']
+DB = Mongo::Client.new([ '163.10.33.220' ], database: 'tesina', connect: :sharded).database
+SHARD_IPS = ['163.10.33.223','163.10.33.217','163.10.33.224']
